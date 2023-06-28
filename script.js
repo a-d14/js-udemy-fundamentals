36c3feb78bbffe6e170507b55340c3b5fd9a0fec
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2023-06-28 20:32:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Aakash");
+
+let firstName = "Aakash";
+
+let aakash_kuheli = "AK";
+let $function = 27;
+
+let country = "India";
+let continent = "Asia";
+let population = 1500000000;
+
+console.log("Country = " + country + ", continent = " + continent + ", population = " + population);
+console.log(typeof true);
+
+console.log(typeof null); // object --> bug not corrected due to legacy reasons
+
